668e9eab7ea1dc3ed705427e8742ff7f2ce03a88
Kritika Garg <kgarg001@odu.edu>
2021-10-27 14:04:31 -0400

    Add files via upload

---
+++ carousels_demo_impl3/automate_slideshow_impl3.js
@@ -0,0 +1,20 @@
+$(document).ready(function() { 
+
+  $("#slideshow > div:gt(0)").hide();
+  
+  setInterval(function() { 
+
+    $('#img1').attr('src','https://kritikagarg.github.io/Test/img/1.jpg?t=&tsp='+ Math.floor(Date.now() / 1000));
+    $('#img2').attr('src','https://kritikagarg.github.io/Test/img/2.jpg?t=&tsp='+ Math.floor(Date.now() / 1000)); 
+    $('#img3').attr('src','https://kritikagarg.github.io/Test/img/3.jpg?t=&tsp='+ Math.floor(Date.now() / 1000)); 
+      
+    $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+
+  }, 4000);
+  
+});
