572bab7b21cdc502e11645d3d7054f0aa8f7acae
Kritika Garg <kgarg001@odu.edu>
2021-11-04 14:09:43 -0400

    Create automate_slideshow_impl3_01.js

---
+++ carousels_demo_impl3/automate_slideshow_impl3_01.js
@@ -0,0 +1,20 @@
+$(document).ready(function() { 
+
+  $("#slideshow > div:gt(0)").hide();
+  
+  setInterval(function() { 
+
+    $('#img1').attr('src','https://kritikagarg.github.io/Test/img/1.jpg'+ Math.floor(Date.now() / 100000));
+    $('#img2').attr('src','https://kritikagarg.github.io/Test/img/2.jpg'+ Math.floor(Date.now() / 100000)); 
+    $('#img3').attr('src','https://kritikagarg.github.io/Test/img/3.jpg'+ Math.floor(Date.now() / 100000)); 
+      
+    $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+
+  }, 4000);
+  
+});
