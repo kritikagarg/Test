16ff84923728634ad92d1f1e1e9b6e8307e69486
Kritika Garg <kgarg001@odu.edu>
2021-11-04 13:25:35 -0400

    Update automate_slideshow_impl3.js

--- carousels_demo_impl3/automate_slideshow_impl3.js
+++ carousels_demo_impl3/automate_slideshow_impl3.js
@@ -4,9 +4,9 @@ $(document).ready(function() {
   
   setInterval(function() { 
 
-    $('#img1').attr('src','https://kritikagarg.github.io/Test/img/1.jpg?t=&tsp='+ Math.floor(Date.now() / 1000));
-    $('#img2').attr('src','https://kritikagarg.github.io/Test/img/2.jpg?t=&tsp='+ Math.floor(Date.now() / 1000)); 
-    $('#img3').attr('src','https://kritikagarg.github.io/Test/img/3.jpg?t=&tsp='+ Math.floor(Date.now() / 1000)); 
+    $('#img1').attr('src','https://kritikagarg.github.io/Test/img/1.jpg?'+ Math.floor(Date.now() / 1000));
+    $('#img2').attr('src','https://kritikagarg.github.io/Test/img/2.jpg?'+ Math.floor(Date.now() / 1000)); 
+    $('#img3').attr('src','https://kritikagarg.github.io/Test/img/3.jpg?'+ Math.floor(Date.now() / 1000)); 
       
     $('#slideshow > div:first')
     .fadeOut(1000)
